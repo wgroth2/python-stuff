d551df8dfbe9fc8d053826f531d39eb1a870054d
Bill Roth <bill.roth@gmail.com>
2020-07-29 12:00:03 -0700

    Update doclick.py
    added message box

--- doclick.py
+++ doclick.py
@@ -1,6 +1,9 @@
 import pyautogui
 import time
 import urllib.request
+from tkinter import * #required.
+from tkinter import messagebox #for messagebox.
+
 #
 # note py3 required
 #
@@ -9,5 +12,7 @@ def doclick(num):
     pyautogui.click()
     time.sleep(2)
     contents = urllib.request.urlopen("192.168.86.19:81/count.php").read()
-    
-   
+  App = Tk() #required.
+  App.withdraw() #for hide window.
+  messagebox.showinfo("Notification", "Done!") #msgbox
+  App.mainloop() #required.    
